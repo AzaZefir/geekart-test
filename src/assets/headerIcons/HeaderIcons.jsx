6fe3f6dc5aed11d54ffb278b32a09a433122c911
azat <arykov.azat.kg@gmail.com>
2024-06-03 15:55:18 +0600

    fix: and add icon

--- src/assets/headerIcons/HeaderIcons.jsx
+++ src/assets/headerIcons/HeaderIcons.jsx
@@ -249,8 +249,8 @@ export const BurgerMenuIcon = () => {
 export const CloseIcon = () => {
   return (
     <svg
-      width="20"
-      height="20"
+      width="24"
+      height="24"
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
