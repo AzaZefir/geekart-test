a0deaf088ced8435a9f0840f128331a6a0dbcf0d
azat <arykov.azat.kg@gmail.com>
2024-06-04 22:46:51 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base:'/',
-=======
-  base: "/",
->>>>>>> c338e82dad2ad205f3b7d4f1e0c29b15e5ee831a
   plugins: [react()],
 });
