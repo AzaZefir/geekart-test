18e599f2761055bebf47c42780428aef4e8ff2a5
azat <arykov.azat.kg@gmail.com>
2024-06-05 14:06:46 +0600

    add: popover with add photo and delete ui

--- src/components/ui/ChangeUserAvatar.jsx
+++ src/components/ui/ChangeUserAvatar.jsx
@@ -1,11 +1,26 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, Divider, IconButton, Popover, Typography } from "@mui/material";
 import React from "react";
 import {
   FormModalCameraIcon,
   FormUserIcon,
 } from "../../assets/modal/FormUserIcon";
+import { AddPhotoIcon } from "../../assets/profileIcons/ProfileIcons";
+import { DeleteIcon } from "./../../assets/profileIcons/ProfileIcons";
 
 const ChangeUserAvatar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <Box sx={{ m: "16px 24px 32px 24px" }}>
       <Box
@@ -24,6 +39,7 @@ const ChangeUserAvatar = () => {
       >
         <FormUserIcon />
         <IconButton
+          onClick={handleClick}
           sx={{
             position: "absolute",
             left: "68px",
@@ -35,6 +51,54 @@ const ChangeUserAvatar = () => {
         >
           <FormModalCameraIcon />
         </IconButton>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          sx={{
+            top: "15px ",
+            left: "-68px",
+          }}
+        >
+          <Box>
+            <Box
+              sx={{
+                p: "10px 12px ",
+                display: "flex",
+                gap: "8px",
+                alignItems: "center",
+              }}
+            >
+              <AddPhotoIcon />{" "}
+              <Typography
+                sx={{ color: "rgba(6, 8, 44, 1)", fontWeight: "500" }}
+              >
+                Загрузить
+              </Typography>
+            </Box>
+            <Divider />
+            <Box
+              sx={{
+                p: "10px 12px ",
+                display: "flex",
+                gap: "8px",
+                alignItems: "center",
+              }}
+            >
+              <DeleteIcon />{" "}
+              <Typography
+                sx={{ color: "rgba(6, 8, 44, 1)", fontWeight: "500" }}
+              >
+                Удалить
+              </Typography>
+            </Box>
+          </Box>
+        </Popover>
       </Box>
     </Box>
   );
